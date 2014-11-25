6c0d9fe7ef00f82703b631d210d6e39c05efccfb
datoncito <campitos@mexico-movil.com>
2014-11-25 10:52:41 -0600

    Guevos culeros!!!

--- src/main/java/com/jc/elementos/model/ControladorUsuario.java
+++ src/main/java/com/jc/elementos/model/ControladorUsuario.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jc.elementos.controller;
+package com.jc.elementos.model;
 
+import com.jc.elementos.controller.*;
 import com.jc.elementos.model.DAOUsuarioImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
